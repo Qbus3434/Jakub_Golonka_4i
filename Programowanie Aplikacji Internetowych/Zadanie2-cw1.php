e2d18a5400a64511035f3c120a906484c2eba729
JGolonka <90254629+Qbus3434@users.noreply.github.com>
2022-09-13 08:04:25 +0200

    13.09.2022
    13.09.2022

---
+++ Programowanie Aplikacji Internetowych/Zadanie2-cw1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zadanie2 - Ćwiczenie1- 13.09.2022</title>
+</head>
+<body>
+
+    <?php
+        const CENA = 7;
+        define('SPALANIE', 8);
+
+        $trasa = 320;
+
+        $wynik = ($trasa / 100) * SPALANIE * CENA;
+
+        echo "WYNIK=$wynik";
+    ?>
+    
+</body>
+</html>:
